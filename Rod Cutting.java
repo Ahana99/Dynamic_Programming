6a717fd693785a09e4e1631b927224d4ba3a66d5
Ahana99 <99ahana@gmail.com>
2024-11-11 18:43:03 +0530

    10-11-24 DP

--- Rod Cutting.java
+++ Rod Cutting.java
@@ -7,19 +7,31 @@
 import java.util.*;
 public class MyClass {
   public static void main(String args[]) {
-     int[] v = {15, 14, 10, 45, 30}; // Values of the items
-    int n = 5;
-       
+     int[] v = {1,5,8,9,10,17,17,20}; // Values of the items
+    int n = 8;
+       int[] dp = new int[n+1];
+       Arrays.fill(dp, -1);
         // Call the knapsack function and print the result
-         int result = rodSplits(v,n,new int[v.length+1]);
+         int result = rodSplits(v,n,dp);
          System.out.println("Maximum value that can be obtained: " + result);
   }
   public static int rodSplits(int[] v, int n, int[] dp){
       if(n==0){
         return 0;
       }
-      if(dp[n]!=0){
+      if(dp[n]!=-1){
         return dp[n];
       }
+      
+      int maxAmt = Integer.MIN_VALUE;
+      for(int i=1; i<=n; i++){
+          int currentPrice = v[i-1] + rodSplits(v, n-i, dp);
+          maxAmt = Math.max(maxAmt, currentPrice);
+      }
+      
+      dp[n] = maxAmt;
+      
+      return dp[n];
+
   }
 }
