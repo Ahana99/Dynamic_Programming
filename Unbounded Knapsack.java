1df115eaf72ac789904e18748d694273d215b66b
Ahana Bhattacharjee <93568117+Ahana99@users.noreply.github.com>
2024-11-24 11:37:21 +0530

    Unbounded Knapsack.java

--- Unbounded Knapsack.java
+++ Unbounded Knapsack.java
@@ -6,35 +6,34 @@
  * You are required to calculate and print the maximum value that can be created in the bag without overflowing it's capacity.
  * NOTE: Each weight can be taken any number of times.
  */
-
 import java.util.*;
-public class MyClass {
-  public static void main(String args[]) {
-     int[] v = {1,5,8,9,10,17,17,20}; // Values of the items
-    int n = 8;
-       int[] dp = new int[n+1];
-       Arrays.fill(dp, -1);
-        // Call the knapsack function and print the result
-         int result = rodSplits(v,n,dp);
-         System.out.println("Maximum value that can be obtained: " + result);
-  }
-  public static int rodSplits(int[] v, int n, int[] dp){
-      if(n==0){
-        return 0;
-      }
-      if(dp[n]!=-1){
-        return dp[n];
-      }
-      
-      int maxAmt = Integer.MIN_VALUE;
-      for(int i=1; i<=n; i++){
-          int currentPrice = v[i-1] + rodSplits(v, n-i, dp);
-          maxAmt = Math.max(maxAmt, currentPrice);
-      }
-      
-      dp[n] = maxAmt;
-      
-      return dp[n];
 
-  }
+public class UnboundedKnapsack {
+
+    // Function to find the maximum value that can be obtained in the knapsack
+    public static int unboundedKnapsack(int W, int[] wt, int[] val, int n) {
+        int[] dp = new int[W + 1];  // dp[i] will store the max value for weight i
+        
+        // Process each capacity from 0 to W
+        for (int i = 0; i <= W; i++) {
+            for (int j = 0; j < n; j++) {
+                if (wt[j] <= i) {  // If the weight of the item is less than or equal to the current capacity
+                    dp[i] = Math.max(dp[i], dp[i - wt[j]] + val[j]);
+                }
+            }
+        }
+        
+        return dp[W];  // Maximum value that can be achieved with capacity W
+    }
+
+    public static void main(String[] args) {
+        // Sample inputs
+        int[] wt = {1, 3, 4, 5};  // Weights of the items
+        int[] val = {1, 4, 5, 7};  // Values of the items
+        int W = 7;  // Maximum weight of the knapsack
+        int n = wt.length;  // Number of items
+
+        // Call the function
+        System.out.println("Maximum value in Knapsack: " + unboundedKnapsack(W, wt, val, n));
+    }
 }
