e4abd7232acad6bc964ae2c0617c454f1966359f
Ahana Bhattacharjee <93568117+Ahana99@users.noreply.github.com>
2025-01-15 10:01:58 +0530

    Update 3 Sum Problem.java

--- Data Structures/3 Sum Problem.java
+++ Data Structures/3 Sum Problem.java
@@ -1,39 +1,39 @@
 /**
- * Given an array of integers, find all unique triplets in the array that sum up to zero.class Solution {
+ * Given an array of integers, find all unique triplets in the array that sum up to zero.
 
  */
-
-public List<List<Integer>> threeSum(int[] nums) {
-    List<List<Integer>> result = new ArrayList<>();
-    Arrays.sort(nums);
-    for(int i = 0; i<nums.length-2;i++){
-        if(i>0 && nums[i] == nums[i-1]){
-            continue;
-        }
-
-        int left = i+1, right = nums.length-1;
-        while(left<right){
-            int sum = nums[i] + nums[left] + nums[right];
-            if(sum == 0){
-                result.add(Arrays.asList(nums[i], nums[left], nums[right]));
-                while(left<right && nums[left] == nums[left+1]){
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+        for(int i = 0; i<nums.length-2;i++){
+            if(i>0 && nums[i] == nums[i-1]){
+                continue;
+            }
+    
+            int left = i+1, right = nums.length-1;
+            while(left<right){
+                int sum = nums[i] + nums[left] + nums[right];
+                if(sum == 0){
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    while(left<right && nums[left] == nums[left+1]){
+                        left++;
+                    }
+                    while(right>left && nums[right] == nums[right-1]){
+                        right--;
+                    }
+    
                     left++;
-                }
-                while(right>left && nums[right] == nums[right-1]){
                     right--;
                 }
-
-                left++;
-                right--;
-            }
-            else if(sum<0){
-                left++;
-            }
-            else{
-                right--;
+                else if(sum<0){
+                    left++;
+                }
+                else{
+                    right--;
+                }
             }
         }
+        return result;
     }
-    return result;
-}
 }
